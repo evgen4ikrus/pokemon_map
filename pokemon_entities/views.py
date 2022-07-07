aa103fd647b761a3d7f477286a0bfcdf7581e2b8
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-07-07 18:50:48 +0300

    Подключил модели к странице покемона

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -55,24 +55,22 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
-    for pokemon in pokemons:
-        if pokemon['pokemon_id'] == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
-    else:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
-
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    pokemon_entitys = PokemonEntity.objects.filter(pokemon=pokemon)
+    
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
+    for pokemon_entity in pokemon_entitys:
         add_pokemon(
-            folium_map, pokemon_entity['lat'],
-            pokemon_entity['lon'],
-            pokemon['img_url']
+            folium_map, pokemon_entity.lat,
+            pokemon_entity.lon,
+            request.build_absolute_uri(f'/media/{pokemon_entity.pokemon.image}')
         )
 
+    pokemon = {
+        'title_ru': pokemon.title,
+        'img_url': request.build_absolute_uri(f'/media/{pokemon.image}')
+    }
+    
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(), 'pokemon': pokemon
     })
