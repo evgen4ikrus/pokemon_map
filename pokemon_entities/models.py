de48a9d8f685c9e59da83790e62b49ef2ab9dbd1
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-07-08 19:04:45 +0300

    Задал русскоязычные названия полей моделей в админке

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,23 +3,24 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(blank=True,)
+    title = models.CharField(max_length=200, verbose_name='Название на русском')
+    title_en = models.CharField(max_length=200, verbose_name='Название на английском')
+    title_jp = models.CharField(max_length=200, verbose_name='Название на японском')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(blank=True, verbose_name='Картинка')
     previous_evolution = models.ForeignKey(
         "self",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         related_name='next_evolution',
+        verbose_name='Из кого эволюционирует'
         )
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strenght = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    level = models.IntegerField(verbose_name='Уровень')
+    health = models.IntegerField(verbose_name='Здоровье')
+    strenght = models.IntegerField(verbose_name='Атака')
+    defence = models.IntegerField(verbose_name='Защита')
+    stamina = models.IntegerField(verbose_name='Выносливость')
     def __str__(self):
         return self.title
     
@@ -28,10 +29,11 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
+        verbose_name='Название покемона'
     )
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
+    lat = models.FloatField(verbose_name='Широта на карте')
+    lon = models.FloatField(verbose_name='Долгота на карте')
+    appeared_at = models.DateTimeField(verbose_name='Когда покемон появится')
+    disappeared_at = models.DateTimeField(verbose_name='Когда покемон исчезнет')
     def __str__(self):
         return f'{self.pokemon} находится по координатам: {self.lat}, {self.lon}'
