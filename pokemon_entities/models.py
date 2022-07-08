e7f09bf05a6ade8e8cfab2b2756b8993a6b40cbe
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-07-08 19:18:17 +0300

    Сделал некоторые поля моделей необязательными

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,13 +1,12 @@
-from tkinter import CASCADE
-from django.db import models  # noqa F401
+from django.db import models 
 
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название на русском')
-    title_en = models.CharField(max_length=200, verbose_name='Название на английском')
-    title_jp = models.CharField(max_length=200, verbose_name='Название на японском')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(blank=True, verbose_name='Картинка')
+    title_en = models.CharField(max_length=200, verbose_name='Название на английском', blank=True)
+    title_jp = models.CharField(max_length=200, verbose_name='Название на японском', blank=True)
+    description = models.TextField(verbose_name='Описание', blank=True)
+    image = models.ImageField(verbose_name='Картинка', blank=True, null=True)
     previous_evolution = models.ForeignKey(
         "self",
         on_delete=models.CASCADE,
@@ -16,11 +15,13 @@ class Pokemon(models.Model):
         related_name='next_evolution',
         verbose_name='Из кого эволюционирует'
         )
-    level = models.IntegerField(verbose_name='Уровень')
-    health = models.IntegerField(verbose_name='Здоровье')
-    strenght = models.IntegerField(verbose_name='Атака')
-    defence = models.IntegerField(verbose_name='Защита')
-    stamina = models.IntegerField(verbose_name='Выносливость')
+    
+    level = models.IntegerField(verbose_name='Уровень', null=True, blank=True)
+    health = models.IntegerField(verbose_name='Здоровье', null=True, blank=True)
+    strenght = models.IntegerField(verbose_name='Атака', null=True, blank=True)
+    defence = models.IntegerField(verbose_name='Защита', null=True, blank=True)
+    stamina = models.IntegerField(verbose_name='Выносливость', null=True, blank=True)
+    
     def __str__(self):
         return self.title
     
@@ -33,7 +34,8 @@ class PokemonEntity(models.Model):
     )
     lat = models.FloatField(verbose_name='Широта на карте')
     lon = models.FloatField(verbose_name='Долгота на карте')
-    appeared_at = models.DateTimeField(verbose_name='Когда покемон появится')
-    disappeared_at = models.DateTimeField(verbose_name='Когда покемон исчезнет')
+    appeared_at = models.DateTimeField(verbose_name='Когда покемон появится', null=True, blank=True)
+    disappeared_at = models.DateTimeField(verbose_name='Когда покемон исчезнет', null=True, blank=True)
+    
     def __str__(self):
         return f'{self.pokemon} находится по координатам: {self.lat}, {self.lon}'
